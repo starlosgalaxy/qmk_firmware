e505b4a8f8b0c8a143253b6c5af99be18b64a359
carlos salazar <carlosgsalazarf@gmail.com>
2025-06-02 18:43:56 -0500

    home row mods + 6th column keys added

--- keyboards/input_club/ergodox_infinity/keymaps/starlosgalaxy/keymap.c
+++ keyboards/input_club/ergodox_infinity/keymaps/starlosgalaxy/keymap.c
@@ -37,41 +37,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | M0(2)|           | M0(2)|   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |QK_GESC |   Q  |   W  |   E  |   R  |   T  | MO(2)|           | MO(2)|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |HOME_A|HOME_S|HOME_D|HOME_F|   G  |------|           |------|   H  |HOME_J|HOME_K|HOME_L|H_SCLN|  '   |
+ * | Tab    |HOME_A|HOME_S|HOME_D|HOME_F|   G  |------|           |------|   H  |HOME_J|HOME_K|HOME_L|H_SCLN|   '    |
  * |--------+------+------+------+------+------| MO(1)|           | MO(1)|------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RAlt   |
+ * |    [   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   ]    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | LGui |      |       |      |QK_GESC|
+ *                                        | MO(1)| MO(2)|       | MO(2)| MO(1)|
  *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |BkSp  |Esc   |------|       |------|Enter |Space |
  *                                 |      |      | MO(3)|       | MO(3)|      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 | BkSp |      | MO(3)|       | MO(3)| Enter|  Space|
  *                                 `--------------------'       `--------------------'
  */
 [BASE] = LAYOUT_ergodox(
     // left hand
-    KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(2),
-    KC_LCTL,HOME_A, HOME_S,  HOME_D,  HOME_F,    KC_G,
-    KC_LSFT, KC_Z, KC_X, KC_C,  KC_V,    KC_B,    MO(1),
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                        KC_LGUI,  KC_NO,
-                                                KC_NO,
-                                KC_BSPC,  KC_NO, MO(3),
+      KC_NO,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
+    QK_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(2),
+     KC_TAB,HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,
+    KC_LBRC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(1),
+      KN_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                         MO(1),    MO(2),
+                                                   KN_NO,
+                                KC_BSPC,  KC_ESC,  MO(3),
     // right hand
-    KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
-    MO(2),   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-             KC_H,  HOME_J,  HOME_K,  HOME_L,HOME_SCLN,   KC_QUOT,
-    MO(1),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
-                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO, QK_GESC,
-    KC_NO,
-    MO(3),   KC_ENT, KC_SPC
+    KC_NO,    KC_6,  KC_7,   KC_8,  KC_9,      KC_0,    KC_NO,
+    MO(2),    KC_Y,  KC_U,   KC_I,  KC_O,      KC_P,  KC_BSLS,
+              KC_H,HOME_J, HOME_K, HOME_L,HOME_SCLN,  KC_QUOT,
+    MO(1),    KC_N,  KC_M,KC_COMM, KC_DOT,  KC_SLSH,  KC_RBRC,
+                    KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,
+    MO(2),    MO(1),
+    KN_NO,
+    MO(3),   KC_ENT,  KC_SPC
 ),
 
 /* Keymap 1: Symbol Layer
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   #  |   $  |   %  |      |           |      |   ^  |   &  |   *  |   (  |   )  |   \    |
+ * |        |   !  |   @  |   #  |   $  |   %  |      |           |      |   ^  |   &  |   *  |   (  |   )  |   \     |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |   1  |   2  |   3  |   4  |   5  |------|           |------|   -  |   =  |   `  |   [  |   ]  |   |    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -97,22 +97,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMB] = LAYOUT_ergodox(
     // left hand
-    KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-    KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS,
-    KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-    KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                        KC_TRNS, KC_NO,
-                                                KC_NO,
-                                KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_NO,
+    KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_TRNS,
+    KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+    KC_TRNS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_TRNS,
+    KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                        KC_TRNS,  KC_TRNS,
+                                                  KC_TRNS,
+                                KC_TRNS,KC_TRNS,  KC_TRNS,
     // right hand
-    KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+      KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_NO,
     KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
-             KC_MINS, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC, KC_PIPE,
-    KC_TRNS, KC_UNDS, KC_PLUS, KC_TILD, KC_LCBR, KC_RCBR, KC_TRNS,
-                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,   KC_NO,
-    KC_NO,
+             KC_MINS, KC_EQL,   KC_GRV, KC_LBRC, KC_RBRC, KC_PIPE,
+    KC_TRNS, KC_UNDS,KC_PLUS,  KC_TILD, KC_LCBR, KC_RCBR, KC_TRNS,
+                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_TRNS, KC_TRNS,
+    KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
@@ -139,23 +139,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [MDIA] = LAYOUT_ergodox(
     // left hand
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
-    KC_NO,   KC_MPRV, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY,
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_TRNS,
+    KC_NO, KC_MPRV, KC_MNXT, KC_VOLU, KC_VOLD,  KC_MPLY,
+    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_TRNS,
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                        KC_NO,   KC_NO,
-                                                KC_NO,
-                                KC_NO,   KC_NO,   KC_TRNS,
+                                        KC_TRNS,KC_TRNS,
+                                                KC_TRNS,
+                                KC_TRNS,KC_TRNS,KC_TRNS,
     // right hand
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,
-             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,
-    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_TRNS,  KC_F6,  KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_NO,
+            KC_LEFT,KC_DOWN,   KC_UP, KC_RGHT,   KC_NO,   KC_NO,
+    KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,   KC_NO,
-    KC_NO,
-    KC_TRNS, KC_NO,   KC_NO
+    KC_TRNS,KC_TRNS,
+    KC_TRNS,
+    KC_TRNS, KC_TRNS,KC_TRNS
 ),
 
 /* Keymap 3: Reset Layer
@@ -187,8 +187,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                                         KC_NO,   KC_NO,
-                                                KC_NO,
-                                KC_NO,   KC_NO,   KC_TRNS,
+                                                 KC_NO,
+                                KC_NO,   KC_NO,KC_TRNS,
     // right hand
     QK_BOOT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
