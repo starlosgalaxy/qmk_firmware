521586f6771def06281393d2a373d119f64b8b23
carlos salazar <carlosgsalazarf@gmail.com>
2025-06-03 12:11:21 -0500

    Update keymap for Ergodox Infinity: swap brackets and adjust right hand layout for improved functionality

--- keyboards/input_club/ergodox_infinity/keymaps/starlosgalaxy/keymap.c
+++ keyboards/input_club/ergodox_infinity/keymaps/starlosgalaxy/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Tab    |HOME_A|HOME_S|HOME_D|HOME_F|   G  |------|           |------|   H  |HOME_J|HOME_K|HOME_L|H_SCLN|   '    |
  * |--------+------+------+------+------+------| MO(1)|           | MO(1)|------+------+------+------+------+--------|
- * |    [   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   ]    |
+ * |    ]   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   [    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
     QK_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(2),
      KC_TAB,HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,
-    KC_LBRC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(1),
+    KC_RBRC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(1),
       KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
                                          MO(1),    MO(2),
                                                    KC_NO,
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,    KC_6,  KC_7,   KC_8,  KC_9,      KC_0,    KC_NO,
     MO(2),    KC_Y,  KC_U,   KC_I,  KC_O,      KC_P,  KC_BSLS,
               KC_H,HOME_J, HOME_K, HOME_L,HOME_SCLN,  KC_QUOT,
-    MO(1),    KC_N,  KC_M,KC_COMM, KC_DOT,  KC_SLSH,  KC_RBRC,
+    MO(1),    KC_N,  KC_M,KC_COMM, KC_DOT,  KC_SLSH,  KC_LBRC,
                     KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,
     MO(2),    MO(1),
     KC_NO,
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  | F10  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        | Prev | Next | Vol+ | Vol- | Play |------|           |------| Left | Down |  Up  |Right |      |        |
+ * |        | Prev | Next | Vol+ | Vol- | Play |------|           |------|      | Left | Down | Up   |Right |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -151,7 +151,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
       KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_TRNS,  KC_F6,  KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_NO,
-            KC_LEFT,KC_DOWN,   KC_UP, KC_RGHT,   KC_NO,   KC_NO,
+              KC_NO,KC_LEFT,   KC_DOWN, KC_UP, KC_RGHT,   KC_NO,
     KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
     KC_TRNS,KC_TRNS,
