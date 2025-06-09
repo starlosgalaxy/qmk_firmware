eb1c63e428d5b94385503fded89eafbd9620ef4d
Carlos Salazar <carlos.salazar@slalom.com>
2025-06-09 11:40:40 -0500

    Add Visual Studio solution file and update VSCode settings to disable format on save

--- keyboards/input_club/ergodox_infinity/keymaps/starlosgalaxy/keymap.c
+++ keyboards/input_club/ergodox_infinity/keymaps/starlosgalaxy/keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |QK_GESC |   Q  |   W  |   E  |   R  |   T  | MO(2)|           | MO(2)|   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Tab    |HOME_A|HOME_S|HOME_D|HOME_F|   G  |------|           |------|   H  |HOME_J|HOME_K|HOME_L|H_SCLN|   '    |
- * |--------+------+------+------+------+------| MO(1)|           | MO(1)|------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| MO(2)|           | MO(2)|------+------+------+------+------+--------|
  * |    ]   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   [    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   |      |      |      |      |      |                                       | Left | Down | Up   | Right|      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | MO(1)| MO(2)|       | MO(2)| MO(1)|
@@ -59,17 +59,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
     QK_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(2),
      KC_TAB,HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,
-    KC_RBRC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(1),
+    KC_RBRC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(2),
       KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                         MO(1),    MO(2),
+                                           MO(1),    MO(2),
                                                    KC_NO,
                                 KC_BSPC,  KC_ESC,  MO(3),
     // right hand
-    KC_NO,    KC_6,  KC_7,   KC_8,  KC_9,      KC_0,    KC_NO,
-    MO(2),    KC_Y,  KC_U,   KC_I,  KC_O,      KC_P,  KC_BSLS,
-              KC_H,HOME_J, HOME_K, HOME_L,HOME_SCLN,  KC_QUOT,
-    MO(1),    KC_N,  KC_M,KC_COMM, KC_DOT,  KC_SLSH,  KC_LBRC,
-                    KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,
+    KC_NO,    KC_6,   KC_7,   KC_8,  KC_9,      KC_0,    KC_NO,
+    MO(2),    KC_Y,   KC_U,   KC_I,  KC_O,      KC_P,  KC_BSLS,
+                  KC_H, HOME_J, HOME_K, HOME_L,HOME_SCLN,  KC_QUOT,
+    MO(2),    KC_N,   KC_M,KC_COMM, KC_DOT,  KC_SLSH,  KC_LBRC,
+                           KC_LEFT,KC_DOWN,  KC_UP,  KC_RGHT,    KC_NO,
     MO(2),    MO(1),
     KC_NO,
     MO(3),   KC_ENT,  KC_SPC
@@ -103,15 +103,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,  HOME_1,  HOME_2,  HOME_3,  HOME_4,    KC_5,
     KC_TRNS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_TRNS,
     KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                        KC_TRNS,  KC_TRNS,
-                                                  KC_TRNS,
-                                KC_TRNS,KC_TRNS,  KC_TRNS,
+                                          KC_TRNS,  KC_TRNS,
+                                                        KC_TRNS,
+                             KC_TRNS, KC_TRNS,  KC_TRNS,
     // right hand
       KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_NO,
     KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
              KC_MINS, KC_EQL,   KC_GRV, KC_LBRC, KC_RBRC, KC_PIPE,
     KC_TRNS, KC_UNDS,KC_PLUS,  KC_TILD, KC_LCBR, KC_RCBR, KC_TRNS,
-                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                             KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,   KC_NO,
     KC_TRNS, KC_TRNS,
     KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS
@@ -149,11 +149,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 KC_TRNS,
                                 KC_TRNS,KC_TRNS,KC_TRNS,
     // right hand
-      KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_TRNS,  KC_F6,  KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_NO,
-              KC_NO,KC_LEFT,   KC_DOWN, KC_UP, KC_RGHT,   KC_NO,
-    KC_TRNS,  KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_NO,  KC_NO,  KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_TRNS,  KC_F6,  KC_F7,     KC_F8,   KC_F9,  KC_F10,   KC_NO,
+                  KC_NO,KC_LEFT,   KC_DOWN,   KC_UP, KC_RGHT,   KC_NO,
+    KC_TRNS,  KC_NO,  KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                            KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,   KC_NO,
     KC_TRNS,KC_TRNS,
     KC_TRNS,
     KC_TRNS, KC_TRNS,KC_TRNS
